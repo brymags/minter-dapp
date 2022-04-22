dad737221f6b124a98a4abf50d52c5534c96886b
Bryane Magsalin <bryanemagsalin@yahoo.com>
2022-04-22 21:19:42 +0800

    update frontend

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,9 +1,9 @@
-const contractAddress = "0xBb2FA65349FA6246bC045445967330a9a352A53E"; //Replace with your own contract address
+const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the Zodiac Gems (Feminine) NFT Project!!";
+const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The Zodiac Gems (Feminine) NFT Project is a collection of 9,999 generative art of women that epitomize the 12 zodiac signs.';
+const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
