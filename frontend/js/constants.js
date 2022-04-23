1507980854b14ead92a866fd03e68b97ac4c552f
Bryane Magsalin <bryanemagsalin@yahoo.com>
2022-04-23 17:35:39 +0800

    update reveal and contract add

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "0x2359c507eB48317FE0026D42D7a250D38015F5E2"; //Replace with your own contract address
+const contractAddress = "0xDbda38dF47911f69489Ae154e3EE46b57d3a1a96"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Zodiac Gems (Feminine) NFT Project!!";
